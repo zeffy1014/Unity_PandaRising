4b47390ed43e37d6d2a0491017c7ce3b56e21905
zeffy1014 <zeffy1014@gmail.com>
2020-07-26 05:40:09 +0900

    [add]新規追加
    別プロジェクトをベースに新規作成 メインとなるシーンのUIを仮決め 各種オブジェクトに対するスクリプトは現在軒並みmissing

---
+++ Assets/Scripts/YesNoDialog.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class YesNoDialog : MonoBehaviour
+{
+    private Text title;
+    private Text description;
+    private Button yesButton;
+    private Button noButton;
+
+    private static readonly string PREFAB_NAME = "YesNoDialogPrefab";
+    private static GameObject prefab;
+
+    public static YesNoDialog ShowDialog(
+        string title,        // ダイアログ上部のタイトル
+        string description,  // ダイアログ本文
+        string yes = null,   // Yesボタンの記述(無ければデフォルトの「はい」)
+        string no = null     // Noボタンの記述(無ければデフォルトの「いいえ」)
+    )
+    {
+        if (null == prefab)
+        {
+            prefab = (GameObject)Resources.Load(PREFAB_NAME);
+        }
+
+        var instance = Instantiate(prefab);
+        var handler = instance.GetComponent<YesNoDialog>();
+
+        // 各種文字を設定
+        // TODO:文字数に応じてサイズを変えたい
+        handler.title.text = title;
+        handler.description.text = description;
+
+        if (!string.IsNullOrEmpty(yes))
+        {
+            handler.yesButton.GetComponentInChildren<Text>().text = yes;
+        }
+        handler.yesButton.onClick.AddListener(() => Destroy(handler.gameObject));
+
+        if (!string.IsNullOrEmpty(no))
+        {
+            handler.noButton.GetComponentInChildren<Text>().text = no;
+        }
+        handler.noButton.onClick.AddListener(() => Destroy(handler.gameObject));
+
+        return handler;
+    }
+
+    // Yesボタンの処理登録
+    public void SetYesAction(UnityAction action)
+    {
+        this.yesButton.onClick.AddListener(action);
+    }
+
+    // Noボタンの処理登録
+    public void SetNoAction(UnityAction action)
+    {
+        this.noButton.onClick.AddListener(action);
+    }
+}
